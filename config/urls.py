c08b0f7533f686fe1c0a5f62fe3da36d0c36e70f
Aaron Stockdale <shadowstock76@gmail.com>
2022-11-16 16:06:11 -0500

    Urls nearly completed, working on views

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+from django.urls import path, include
+from hyena import views
+from hyena.models import SocialCard
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('hyena.urls')),
+    
+    
 ]
--- hyena/views.py
+++ hyena/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import permissions, generics
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import SocialCard
+from .serializers import SocialCardSerializer, CommentsSerializer, UserSerializer, StyleSerializer
 
 # Create your views here.
+
+class SocialCardView(generics.ListCreateAPIView):
+    queryset = SocialCard.objects.all()
+    serializer_class = SocialCardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.SocialCards.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
