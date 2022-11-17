25405180493beb68b724afc690f63c3816c64968
Jacob Gilbert <104043847+jacobgilb03@users.noreply.github.com>
2022-11-17 15:56:06 -0500

    Update urls.py

--- hyena/urls.py
+++ hyena/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-path('', views.api_root),
-path('ecard/', views.CardList.as_view(), name='ecard_list'),
-path('cards/',views.CardView.as_view(), name='card-list'),
-path('cards/me/', include('rest_framework.urls')),
-path('cards/all/', views.BookView.as_view(), name='book-list'),
-path('cards/:id', views.BookViewSet.as_view({'get': 'list'}), name='book-list'),
-path('friends/', views.NoteView.as_view(), name='note-list'),
-path('friends/:user_id/', views.NoteView.as_view(), name='note-add'),
+urlpatterns = [
+  path('', views.api_root),
+  path('ecard/', views.CardList.as_view(), name='ecard_list')
+]
+
 
