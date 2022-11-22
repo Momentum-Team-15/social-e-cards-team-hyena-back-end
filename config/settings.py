70a7b6a5948d7b33adb052f6eab4849c7c0234d7
Aaron Stockdale <shadowstock76@gmail.com>
2022-11-21 19:09:49 -0500

    Working on auth code

--- config/settings.py
+++ config/settings.py
@@ -159,10 +159,11 @@ DJOSER = {
 AUTH_USER_MODEL = 'hyena.CustomUser'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+    
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
