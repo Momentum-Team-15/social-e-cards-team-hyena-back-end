d8629f1ebbf6832c3d5d26ffef5c165ce47073a3
Aaron Stockdale <shadowstock76@gmail.com>
2022-11-28 12:20:37 -0500

    Still trying deploy

--- hyena/models.py
+++ hyena/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to="user_avatars", blank=True, null=True)
-    favorites = models.ManyToManyField('SocialCard', related_name='favorite_ecards', blank=True)
     def __str__(self):
         return f'{self.username}'
 
