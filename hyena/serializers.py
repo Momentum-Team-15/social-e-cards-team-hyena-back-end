4d9271aac784ddc44367a188b9802a970c561ece
Aaron Stockdale <shadowstock76@gmail.com>
2022-11-28 11:37:42 -0500

    Added some endpoints but have some cleaning up to do, gonna have merge conflicts

--- hyena/serializers.py
+++ hyena/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser, SocialCard, Comments
+from .models import CustomUser, SocialCard, Favorite
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,22 +7,42 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('id', 'username', 'first_name', 'last_name',)
 
-class SocialCardSerializer(serializers.ModelSerializer):
-    owner = serializers.SlugRelatedField(
-        slug_field="username", read_only=True)
-    
-    class Meta:
-        model = SocialCard
-        fields = ('id', 'owner', 'title', 'border_choices', 'border_color',
-                'card_color', 'font', 'text_align', 'front_message', 'back_message', 'created_date')
-
 class UserCreateSerializer(serializers.ModelSerializer):
     friends = serializers.StringRelatedField(many=True, read_only=True)
     followers = serializers.StringRelatedField(many=True, read_only=True)
     class Meta:
         pass
 
-class CommentsSerializer(serializers.ModelSerializer):
+class SocialCardSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+    
     class Meta:
-        model = Comments
-        fields = ('id', 'user', 'comment', 'card')
+        model = SocialCard
+        fields = ('__all__',)
+
+class SocialCardListSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(slug_field='username', read_only=True)
+    owner_pk = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SocialCard
+        fields = ('id', 'owner', 'owner_pk', 'title', 'border_choices', 'border_color',
+                'card_color', 'font', 'text_align', 'front_message', 'back_message', 'created_date')
+    
+    def get_owner_pk(self, obj):
+        return obj.owner.id
+
+class ModSocialCardSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+    
+    class Meta:
+        model = SocialCard
+        fields = ('id', 'owner')
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ('id', 'card','user','created_at')
--- hyena/urls.py
+++ hyena/urls.py
@@ -3,13 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.api_root),
-    path('ecard/', views.CardList.as_view(), name='ecard_list'),
+    path('ecard/all/', views.AllCardList.as_view(), name='all-ecards'),
+    path('ecard/mine/', views.MyCardList.as_view(), name='my-ecards'),
     path('ecard/<int:pk>/', views.CardDetail.as_view(), name='ecard_detail'),
-    path('comments/', views.CommentsList.as_view(), name='comments-list'),
-    path('comments/<int:pk>/', views.CommentsDetail.as_view(), name='comments-detail'),
     path('users/', views.UserView.as_view(), name='user_profile'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
     path('users/search/', views.UserSearchList.as_view(), name='search_user'),
+    path('favorites/', views.FavoriteCreateView.as_view(), name='favorite-list'),
+    path('favorites/<int:pk>/', views.FavoriteDetailView.as_view(), name='favorite-detail'),
 ]
 
 
