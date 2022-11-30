8e174a3e96f25563b09388368c15772c2e24f2f8
Jacob Gilbert <104043847+jacobgilb03@users.noreply.github.com>
2022-11-30 11:13:21 -0500

    Update models.py

--- hyena/models.py
+++ hyena/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(upload_to="user_avatars", blank=True, null=True)
     
     def __str__(self):
         return f'{self.username}'
